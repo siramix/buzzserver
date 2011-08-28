f3ebcefe5b6f86be0b22773219df40c313ef4587
Patrick Reynolds <cpreynolds@gmail.com>
2011-08-28 18:07:26 -0400

    Adding a script to add a card to get database.
    Adding a card makes all other cards from that game inactive.

---
+++ addcard.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once 'config.php';
+
+header( 'Content-Type:application/json' );
+
+$token = $_REQUEST['token'];
+$title = $_REQUEST['title'];
+$badwords = $_REQUEST['badwords'];
+
+$dd = new PDO('mysql:host=localhost;dbname=siramix_buzzing', $u, $p);
+
+$sql = 'SELECT id FROM games WHERE token= ? ';
+$sth = $dd->prepare($sql);
+if($sth->execute( array($token) ))
+  {
+  $sth->setFetchMode(PDO::FETCH_BOTH);
+  $r = $sth->fetch();
+  if($r)
+    {
+    $id = $r['id'];
+    $sql = 'INSERT INTO cards (game,title,badwords,active) VALUES (?,?,?,?)';
+    $sth = $dd->prepare($sql);
+    if($sth->execute( array($id,$title,$badwords,true) ))
+      {
+      $qid = $dd->lastInsertId();
+      $sql = 'UPDATE cards SET active=? WHERE game=? AND id!=?';
+      $sth = $dd->prepare($sql);
+      if( $sth->execute( array(false,$id,$qid) ))
+        {
+        print json_encode(array('status'=>1));
+        }
+      else
+        {
+        print json_encode(array('status'=>0));
+        }
+      }
+    else
+      {
+      print json_encode(array('status'=>0));
+      }
+    }
+  else
+    {
+    print json_encode(array('status'=>0));
+    }
+  }
+else
+  {
+  print json_encode(array('status'=>0));
+  }
+
+?>
