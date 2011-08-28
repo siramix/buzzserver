34019677c60331e90726ff6376bfaca11b5aa433
Patrick Reynolds <cpreynolds@gmail.com>
2011-08-28 16:52:05 -0400

    Updating the db and making start work properly with buzzing.

--- db.sql
+++ db.sql
@@ -2,12 +2,13 @@ DROP TABLE IF EXISTS games;
 CREATE TABLE games(
      id INT NOT NULL AUTO_INCREMENT,
      token VARCHAR(5) NOT NULL,
+     buzzing BOOLEAN NOT NULL,
      primary key ( id ) );
 
 DROP TABLE IF EXISTS words;
-CREATE TABLE word(
+CREATE TABLE words(
      id INT NOT NULL AUTO_INCREMENT,
      game INT NOT NULL,
-     wordid INT NOT NULL,
+     wordtext TEXT NOT NULL,
      status INT NOT NULL,
      primary key ( id ) );
--- start.php
+++ start.php
@@ -2,25 +2,33 @@
 
 require_once 'config.php';
 
-header( 'Content-Type:application/json' );
-
 function generateRandomString($length) {
   $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
   $string = '';
   for ($p = 0; $p < $length; $p++) {
-    $string .= $characters[mt_rand(0, strlen($characters))];
+    $string .= $characters[mt_rand(0, strlen($characters)-1)];
   }
   return $string;
 }
 
+header( 'Content-Type:application/json' );
+
 $dd = new PDO('mysql:host=localhost;dbname=siramix_buzzing', $u, $p);
 $token = generateRandomString(5);
-$sql = 'INSERT INTO games (token) VALUES ('.$token.')';
+$sql = 'INSERT INTO games (token,buzzing) VALUES (?,?)';
 $sth = $dd->prepare($sql);
-$sth->execute( array() );
-$qid = $dd->lastInsertId();
+if($sth->execute( array($token,false) ))
+  {
+  $qid = $dd->lastInsertId();
 
-$arr = array('id' => $qid,
-             'token' => $token);
-print json_encode($arr);
+  $arr = array('id' => $qid,
+               'token' => $token,
+               'status' => 1);
+  print json_encode($arr);
+  }
+else
+  {
+  print json_encode(array('status'=>0));
+  }
+
 ?>
