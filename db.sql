a6cbee8feccd688c9ee89aabbfef78bc761d2a09
Patrick Reynolds <cpreynolds@gmail.com>
2011-09-03 23:32:19 -0400

    Changing the database to fit the new paradigm.

--- db.sql
+++ db.sql
@@ -1,7 +1,11 @@
 DROP TABLE IF EXISTS games;
 CREATE TABLE games(
      id INT NOT NULL AUTO_INCREMENT,
-     token VARCHAR(5) NOT NULL,
+     player1_id INT NOT NULL,
+     player2_id INT NOT NULL,
+     token VARCHAR(10) NOT NULL UNIQUE,
+     location Point NOT NULL,
+     start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
      buzzing BOOLEAN NOT NULL,
      primary key ( id ) );
 
@@ -14,4 +18,10 @@ CREATE TABLE cards(
      title TEXT NOT NULL,
      badwords TEXT NOT NULL,
      active BOOLEAN NOT NULL,
+     primary key ( id ) );
+
+DROP TABLE IF EXISTS players;
+CREATE TABLE players(
+     id INT NOT NULL AUTO_INCREMENT,
+     token VARCHAR(5) NOT NULL UNIQUE,
      primary key ( id ) );
